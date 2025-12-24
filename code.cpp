3c400d7ab299ec346d963217114f576834a680cf
Arti05-Ambre <ambrearti62@gmail.com>
2025-12-24 10:39:57 +0530

    cpp code

---
+++ code.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int age = 23;
+    char name = 'A';
+    float PI = 3.14f;
+    bool issafe = true;
+    double price = 100.99;
+    cout << age <<endl;
+    return 0;
+}
