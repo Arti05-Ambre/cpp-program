7ddad1ec457e7e1d25f9a25e70e0c6c946b6cedd
Arti05-Ambre <ambrearti62@gmail.com>
2025-12-24 12:07:40 +0530

    operators

--- code.cpp
+++ code.cpp
@@ -1,9 +1,14 @@
 #include<iostream>
 using namespace std;
 int main() {
-    int age;
-    cout<<"enter the age:";
-    cin >> age;
-    cout<<"your age is:"<<age;
+  int a = 20, b = 30;
+  
+  cout<< "sum ="<<(a+b)<< endl;
+  cout<< "difference ="<<(a-b)<< endl;
+  cout<< "Product ="<<(a*b)<< endl;
+  cout<< "division ="<<(a/b)<< endl;
+  cout<< "module ="<<(a%b) << endl;
+
+
     return 0;
 }
