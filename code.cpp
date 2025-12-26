006c31280b58c6bea09e10cd0856a96bef4bf1e1
Arti05-Ambre <ambrearti62@gmail.com>
2025-12-26 15:16:01 +0530

    while loop

--- code.cpp
+++ code.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
  using namespace std;
  int main(){
- int age;
- cout<<"Enter Age:"<< age;
- cin>>age;
- cout<<(age >= 18? "you can vote": "you can't vote" )<< endl;
-
-  return 0;
+ int count=100;
+ while(count <= 200){
+  cout<<count<<" ";
+  count++;
+ }
+ 
   }
