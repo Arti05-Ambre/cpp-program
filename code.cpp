33633332d0a62153a3dbc31e86ea7d317e959bba
Arti05-Ambre <ambrearti62@gmail.com>
2025-12-29 15:19:36 +0530

    sum of number whose divisibl e by 3

--- code.cpp
+++ code.cpp
@@ -1,16 +1,18 @@
 #include<iostream>
  using namespace std;
  int main(){
-  int n = 1;
-  int sum = 0;
-  while(n<=100){
-    if(n%2==0){
-      sum += n;
-    }
-    n++;
-    
+ int n = 1;
+ int sum = 0;
+ while(n<=100){
+  if(n%3==0){
+    sum += n;
   }
-  cout << "print even number:"<<sum<<endl;
+  n++;
+  
+ }
+ cout<<"sum of divisible by 3="<<sum<<endl;
+ 
 
-  return 0;
+ return 0;
 }
+
