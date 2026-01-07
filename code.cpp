6ce2035575109bc4847ffe4001832d231d0f9cc8
Arti05-Ambre <ambrearti62@gmail.com>
2026-01-07 19:22:20 +0530

    floyd's triangle self

--- code.cpp
+++ code.cpp
@@ -2,14 +2,14 @@
  using namespace std;
  int main(){
  int n = 4; 
- int num = 1;
+ char ch = 'A';
  
  for(int i=0; i<n ; i++){
   
     for(int j=i+1; j>0; j--){
-        cout<<num<<" ";
+        cout<<ch<<" ";
         
-        num++;
+        ch++;
        
 
  }
