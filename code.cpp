2ba248bcb4213ba3c9067266066031ec13715012
Arti05-Ambre <ambrearti62@gmail.com>
2026-01-08 15:12:48 +0530

    inverted triangle character

--- code.cpp
+++ code.cpp
@@ -5,18 +5,21 @@
 
  
  for(int i=0; i<n ; i++){
-  
-    for(int j=i; j>=0; j--){
-        char ch = 'A'+j;
-        cout<<ch<<" ";
+  char ch = 'A'+i;
+    for(int j=0; j<i; j++){
         
+        cout<<" ";
+    }
         
+        for(int j=0; j<n-i; j++ ){
+            cout<<ch;
+        }
        
-
- }
  cout<<endl;
  }
+return 0;
+ }
  
- return 0;
-}
+ 
+
 
