22e4041bc01cb40f241312ff5a5cb2932824b2d6
Arti05-Ambre <ambrearti62@gmail.com>
2026-01-09 10:50:59 +0530

    hollow diamond pattern

--- code.cpp
+++ code.cpp
@@ -1,27 +1,56 @@
 #include<iostream>
  using namespace std;
  int main(){
- int n = 8; 
+ int n = 4; 
 
+ //top part
+ for(int i=0; i<n ; i++){//outer loop
  
- for(int i=0; i<n ; i++){
- 
-    for(int j=0; j<n-i-1; j++){
+    for(int j=0; j<n-i-1; j++){//innner loop
         //spaces
         cout<<" ";
     }
-    for(int j=1; j<=i+1; j++ ){
-        //numbers1
-            cout<<j;
+    //print star
+    cout<<"*";
+    if(i !=0){
+
+    
+//again space 
+    for(int j=1; j<=2*i-1; j++ ){
+            cout<<" ";
         }
-        for(int j=i; j>0; j--){
-            //numbers2
-            cout<<j;
-        }
-       
+        //again print star
+        cout<<"*";
+        
+        
+    }
         
  cout<<endl;
  }
+
+
+
+ //Bottom part
+
+ //outer loop
+ for(int i=0; i<n-1; i++ ){// 0 to n-2
+    //spaces
+   
+    for(int j=0; j<i+1; j++){
+        cout<<" ";
+    }
+    cout<<"*";
+
+  if(i != n-2){
+ for(int j=0; j<2*(n-i)-5; j++){
+    cout<<" ";
+ }
+ cout<<"*";
+}
+cout<<endl;
+ }
+ 
+
 return 0;
  }
  
