9022e253ebb93ac4202b0e17b152b0441f766cf3
Arti05-Ambre <ambrearti62@gmail.com>
2026-01-09 12:55:39 +0530

    diamond patterns

--- code.cpp
+++ code.cpp
@@ -2,55 +2,50 @@
  using namespace std;
  int main(){
  int n = 4; 
-
- //top part
- for(int i=0; i<n ; i++){//outer loop
+//upper part
+ //left part
+ for(int i=1; i<=n ; i++){//outer loop
  
-    for(int j=0; j<n-i-1; j++){//innner loop
-        //spaces
+    for(int j=1; j<=i; j++)//innner loop
+    
+        cout<<"*";
+    
+    //spaces
+    for(int j=1; j<=2*(n-i); j++)
         cout<<" ";
+
+
+    //right part
+    for(int j=1; j<=i; j++)//innner loop
+    
+        cout<<"*";
+   cout<<endl;
     }
-    //print star
-    cout<<"*";
-    if(i !=0){
+   
+    
+//bottom part
+
+for(int i=n; i>=1; i--){//outer loop
+ 
+    for(int j=1; j<=i; j++)//innner loop
+    
+        cout<<"*";
+    
+    //spaces
+    for(int j=1; j<=2*(n-i); j++)
+        cout<<" ";
+
 
     
-//again space 
-    for(int j=1; j<=2*i-1; j++ ){
-            cout<<" ";
-        }
-        //again print star
+    for(int j=1; j<=i; j++)//innner loop
+    
         cout<<"*";
-        
-        
-    }
-        
- cout<<endl;
- }
-
-
-
- //Bottom part
-
- //outer loop
- for(int i=0; i<n-1; i++ ){// 0 to n-2
-    //spaces
+        cout<<endl;
    
-    for(int j=0; j<i+1; j++){
-        cout<<" ";
     }
-    cout<<"*";
 
-  if(i != n-2){
- for(int j=0; j<2*(n-i)-5; j++){
-    cout<<" ";
- }
- cout<<"*";
-}
-cout<<endl;
- }
+
  
-
 return 0;
  }
  
