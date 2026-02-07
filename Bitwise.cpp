c153812e29e6215fd87410845880b33c22f4ffd7
Arti05-Ambre <ambrearti62@gmail.com>
2026-02-07 21:02:29 +0530

    pratice example

--- Bitwise.cpp
+++ Bitwise.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
 int main(){
-cout << (6 & 10) <<endl;
-cout << (6 | 10) <<endl;  
- cout << (6 ^ 10) <<endl;
- cout<<(10 << 2)<<endl;
-  cout << (10 >> 1) <<endl;
+cout << sizeof(int) <<endl;
+cout << sizeof(long) <<endl;  
+ cout << sizeof(short)<<endl;
+ cout<<sizeof(long long)<<endl;
+  cout <<sizeof(long int)<<endl;
+   cout <<sizeof(short int)<<endl;
+    cout <<sizeof(signed int)<<endl;
+     cout <<sizeof(unsigned int)<<endl;
  return 0;
 }
