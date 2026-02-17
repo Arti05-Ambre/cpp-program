327736606575f96209d0475e55b567b3b06e0dbc
Arti05-Ambre <ambrearti62@gmail.com>
2026-02-17 13:40:13 +0530

    find smallest value and largest value and index value

--- Array.cpp
+++ Array.cpp
@@ -3,21 +3,29 @@ using namespace std;
 
 int main(){
    int nums[]={1,34,56,78,90,-1,45,-6};
-   int size = 8;
-   
-        int smallest = nums[0];
+   int size = sizeof(nums)/sizeof(nums[0]);
+    int smallest = nums[0];
   int largest = nums[0];
- 
+  int largestindex= 0;
+  int smallestindex = 0;
+  
   for(int i=0; i<size; i++){
-   smallest = min(nums[i],smallest);
-   largest = max(nums[i],largest);
-   
+   if(nums[i]<smallest){
+      smallest = nums[i];
+      smallestindex=i;
+   }
+   if(nums[i]>largest){
+      largest=nums[i];
+      largestindex=i;
+   }
+  
 
    
    }
   
   cout<<"Smallest value:"<<smallest<<endl;
   cout<<"largest value:"<<largest<<endl;
-  
+  cout<<"index valu for small:"<<smallestindex<<endl;
+  cout<<"index valure for large:"<<largestindex<<endl;
 return 0;
 }
