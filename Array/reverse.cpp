9f3c5aafe92aafadb9efca20ee64647aba11fdc0
Arti05-Ambre <ambrearti62@gmail.com>
2026-02-20 17:07:00 +0530

    find size of vector

--- Array/reverse.cpp
+++ Array/reverse.cpp
@@ -1,22 +1,12 @@
  #include<iostream>
+ #include<vector>
  using namespace std;
- void reverse(int arr[],int size){
-    int sum = 0;
-    int product = 1;
-
-  for(int i=0; i<size; i++){
-        sum +=arr[i];
-        product *= arr[i];
-        
- }
- cout<<"sum of array is:"<<sum<<endl;
- cout<<"product of array:"<<product<<endl;
-    }
  int main(){
-    int arr[]={4,5,67,89,2,78,9};
-    int size = 7;
-    reverse(arr,size);
-
-   
+    vector<char>vec={'q','a','d','b','c'};
+    cout<<"size of vector="<<vec.size()<<endl;
+    for(char val:vec){
+        cout<<val<<endl;
+    }
     return 0;
  }
+ 
