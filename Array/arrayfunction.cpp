94e32852f00df467bed44a0ef56ca5f870946e92
Arti05-Ambre <ambrearti62@gmail.com>
2026-02-25 10:47:56 +0530

    push back function

---
+++ Array/arrayfunction.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>vec;
+    cout<<"size="<<vec.size()<<endl;
+    vec.push_back(45);
+ cout<<"size of vector after push back="<<vec.size()<<endl;
+ 
+ return 0;
+}
