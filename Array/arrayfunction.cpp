cb94c0618e740b9590ded7b3b89775e9ac0fe28f
Arti05-Ambre <ambrearti62@gmail.com>
2026-02-25 11:10:09 +0530

    pop  back function

--- Array/arrayfunction.cpp
+++ Array/arrayfunction.cpp
@@ -4,8 +4,19 @@ using namespace std;
 int main(){
     vector<int>vec;
     cout<<"size="<<vec.size()<<endl;
-    vec.push_back(45);
+    vec.push_back(56);
+    vec.push_back(6);
+    vec.push_back(560);
+    vec.push_back(516);
  cout<<"size of vector after push back="<<vec.size()<<endl;
  
+ 
+ vec.pop_back();
+
+ for(int val:vec){
+    cout<<val<<endl;
+
+ }
+
  return 0;
 }
