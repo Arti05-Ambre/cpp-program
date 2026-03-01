e436c873fb6a219d02c16ee5bebe13f57365e1f1
Arti05-Ambre <ambrearti62@gmail.com>
2026-03-01 09:18:32 +0530

    find size of vector and capacity of vector

--- Array/arrayfunction.cpp
+++ Array/arrayfunction.cpp
@@ -3,24 +3,11 @@
 using namespace std;
 int main(){
     vector<int>vec;
-    cout<<"size="<<vec.size()<<endl;
-    vec.push_back(56);
+    
+    vec.push_back(0);
     vec.push_back(6);
     vec.push_back(560);
-    vec.push_back(516);
- cout<<"size of vector after push back="<<vec.size()<<endl;
- 
- 
- vec.pop_back();
- cout<<"Front value in array="<<vec.front()<<endl;
- cout<<"back value in array="<<vec.back()<<endl;
-cout<<"Front value in array="<<vec.at(2)<<endl;
-
-
- for(int val:vec){
-    cout<<val<<endl;
-
- }
-
+    cout<<vec.size()<<endl;
+    cout<<vec.capacity()<<endl;
  return 0;
 }
