a19eea3f2ee62c4aaa99e65a184ae1f09bd2a2a0
Arti05-Ambre <ambrearti62@gmail.com>
2026-03-01 23:14:06 +0530

    new file practice

--- Array/arrayfunction.cpp
+++ Array/arrayfunction.cpp
@@ -4,9 +4,10 @@ using namespace std;
 int main(){
     vector<int>vec;
     vec.push_back(56);
-    vec.push_back(0);
+    vec.push_back(3);
     vec.push_back(6);
     vec.push_back(560);
+    vec.push_back(7);
     cout<<vec.size()<<endl;
     cout<<vec.capacity()<<endl;
  return 0;
---
+++ Array/practice.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int n =4;
+for(int i=0; i<=n; i++){
+    for(int j=0; j<=n; j++){
+        cout<<"j"<<endl;
+
+    }
+cout<<endl;
+}
+return 0;
