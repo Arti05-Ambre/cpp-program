b89a2a23c6b13b2c67c7cba3d0c8500fa6ed69d0
Arti05-Ambre <ambrearti62@gmail.com>
2026-06-02 22:19:33 +0530

    Update calculator.cpp

---
+++ cpp-basic/calculator.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter value of a"<<endl;
+    cin>>a;
+
+    int b;
+    cout<<"Enter value of b"<<endl;
+    cin >>b;
+    cout<<"sum of tu number is:"<< a+b<<endl;
+    cout<<"diff of two number is:"<<a-b<<endl;
+    cout<<"product of two number is:"<<a*b<<endl;
+    cout<<"Division of two number is:"<<a/b<<endl;
+    cout<<"Module of two number is:"<<a%b<<endl;
+
+}
