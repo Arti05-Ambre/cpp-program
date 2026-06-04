bec355f1175686433e9b4779a271d313410fccd2
Arti05-Ambre <ambrearti62@gmail.com>
2026-06-04 13:28:10 +0530

    New file

--- cpp-basic/calculator.cpp
+++ cpp-basic/calculator.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include<iostream>
 using namespace std;
 int main(){
@@ -15,3 +16,22 @@ int main(){
     cout<<"Module of two number is:"<<a%b<<endl;
 
 }
+=======
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter value of a"<<endl;
+    cin>>a;
+
+    int b;
+    cout<<"Enter value of b"<<endl;
+    cin >>b;
+    cout<<"sum of tu number is:"<< a+b<<endl;
+    cout<<"diff of two number is:"<<a-b<<endl;
+    cout<<"product of two number is:"<<a*b<<endl;
+    cout<<"Division of two number is:"<<a/b<<endl;
+    cout<<"Module of two number is:"<<a%b<<endl;
+
+}
+>>>>>>> d87eb2c (my first calculator)
---
+++ cpp-basic/loops.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n = -45;
+    if(n>0){
+        cout<<"Number is Positive"<<endl;
+
+    }
+    else{
+        cout<<"Number is Negative"<<endl;
+    }
+    return 0;
+
+}
