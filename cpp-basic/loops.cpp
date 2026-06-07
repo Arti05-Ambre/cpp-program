6197835af453f5abc4131b8a75f39d20102def02
Arti05-Ambre <ambrearti62@gmail.com>
2026-06-07 22:25:11 +0530

    prit even number from 1 to 20

--- cpp-basic/loops.cpp
+++ cpp-basic/loops.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 
 int main() {
-    for (int i = 1; i <= 10; i++)
-        cout << i << endl;
+    for (int i = 1; i <= 20; i++) {
+        if (i % 2 == 0)
+            cout << i << endl;
+    }
 
     return 0;
 }
